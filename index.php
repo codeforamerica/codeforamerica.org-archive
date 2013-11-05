b1832c8eb69eb8a10558baffd43b83f0538a8271
Anna Debenham <anna@maban.co.uk>
2013-11-05 11:47:10 +0000

    Updating colour mixing to include opacity. Updating profile and highlight patterns.

--- index.php
+++ index.php
@@ -6,22 +6,22 @@
 <h1 class="crate-h1"><?php echo $config['PROJECT_NAME']; ?></h1>
 <h2 class="crate-h2">Project Contacts</h2>
 <div class="section">
-    <div class="half vcard">
+    <div class="half crate vcard">
         <img class="photo" src="_crate/inc/avatar-jeremy-keith.jpg" alt="" width="64" height="64"/>
         <h3 class="crate-h3"><a class="fn url" href="http://clearleft.com/is/jeremy-keith/">Jeremy Keith</a> &#8212; <span class="role">Front-end Development</span></h3>
         <p><a class="email" href="mailto:jeremy@clearleft.com">jeremy@clearleft.com</a></p>
     </div>
-    <div class="half vcard">
+    <div class="half crate vcard">
         <img class="photo" src="_crate/inc/avatar-jon-aizlewood.jpg" alt="" width="64" height="64"/>
         <h3 class="crate-h3"><a class="fn url" href="http://clearleft.com/is/jeremy-keith/">Jon Aizlewood</a> &#8212; <span class="role">Design</span></h3>
         <p><a class="email" href="mailto:jon@clearleft.com">jon@clearleft.com</a></p>
     </div>
-    <div class="half vcard">
+    <div class="half crate vcard">
         <img class="photo" src="_crate/inc/avatar-anna-debenham.jpg" alt="" width="64" height="64"/>
         <h3 class="crate-h3"><a class="fn url" href="http://clearleft.com/is/jeremy-keith/">Anna Debenham</a> &#8212; <span class="role">Front-end Development</span></h3>
         <p><a class="email" href="mailto:anna@clearleft.com">anna@clearleft.com</a></p>
     </div>
-    <div class="half vcard">
+    <div class="half crate vcard">
         <img class="photo" src="_crate/inc/avatar-jessica-jebari.jpg" alt="" width="64" height="64"/>
         <h3 class="crate-h3"><a class="fn url" href="http://clearleft.com/is/jessica-jebari/">Jessica Jebari</a> &#8212; <span class="role">Project Manager</span></h3>
         <p><a class="email" href="mailto:jessica@clearleft.com">jessica@clearleft.com</a></p>
--- styleguide/index.php
+++ styleguide/index.php
@@ -34,12 +34,13 @@
 		$files = array();
 		$patterns_dir = "../_patterns";
 		$handle = opendir($patterns_dir);
+		
 		while (false !== ($file = readdir($handle))):
 			if(stristr($file,'.html')):
 				$files[] = $file;
 			endif;
 		endwhile;
-		rsort($files);
+		sort($files);
 		foreach ($files as $file):
 			echo '<section class="pattern" id="'.str_replace('.html', '', $file).'">';
 			
@@ -47,7 +48,7 @@
 			echo '<summary class="name" title="Show markup and usage"><strong>'.str_replace('.html', '', $file).'</strong> markup and usage </summary>';
 			echo '<section>';
 			echo '<textarea rows="10" cols="30" class="input code">'.htmlspecialchars(file_get_contents($patterns_dir.'/'.$file)).'</textarea>';
-			echo '<p class="caption"><strong>Usage:</strong> '.htmlspecialchars(@file_get_contents($patterns_dir.'/'.str_replace('.html','.txt',$file))).'</p>';
+			echo '<pre class="caption"><strong>Usage:</strong> '.htmlspecialchars(@file_get_contents($patterns_dir.'/'.str_replace('.html','.txt',$file))).'</pre>';
 			echo '</section>';
 			echo '</details><!--/.primer-->';
 			echo '<div class="preview">';
