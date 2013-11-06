ab691de6d72d4a491689c49ef24b50de84d2df00
adactio <jeremy@adactio.com>
2013-11-06 18:08:01 +0000

    output document root (for debugging)

--- _inc/functions.php
+++ _inc/functions.php
@@ -1,5 +1,5 @@
 <?php
-echo 'wtf?';
+
 include_once($_SERVER['DOCUMENT_ROOT'].'/_inc/vendor/paulrobertlloyd/barebones/barebones.inc.php');
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php echo $_SERVER['DOCUMENT_ROOT']; ?>
 <?php
     include_once($_SERVER['DOCUMENT_ROOT'].'/_inc/functions.php');
 
