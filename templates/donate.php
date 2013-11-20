56e44dc5a85527e6f1db054d0d7d646d245861ca
Anna Debenham <anna@maban.co.uk>
2013-11-20 11:44:10 +0000

    Added template skeletons and IE layout

---
+++ templates/donate.php
@@ -0,0 +1,47 @@
+<?php include("../blocks/global/head.html") ?>
+    
+    <div class="masthead">
+        <header class="layout-container masthead-header">
+            <?php include("../blocks/patterns/nav-breadcrumb.html") ?>
+            <h1 class="page-title">Donate Now</h1>
+        </header>
+    </div>
+    
+    <?php include("../blocks/global/header.html") ?>
+    
+    <div class="layout-container">
+    
+        <main role="main">
+            
+            <nav class="nav-breadcrumbs">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">All cities</a></li>
+                    <li><a href="#">Upcoming</a></li>
+                    <li>2013 Fellows</li>
+                </ul>
+            </nav>
+            
+            <h2>Invest in the future of Government</h2>
+                
+                <p>Hundreds of individuals have joined together to invest their time, energy, ideas, and money in our fellows and in our mission. Together we are building a movement that changes the way our cities function -- a movement that helps cities to keep pace with their community and interact with residents using new tools and technologies.</p>
+                
+                <p>Code for America is generously supported by leaders in philanthropy and technology. Their support makes our work possible. It allows for us to connect city governments with web and design talent.</p>
+                
+                <img src="http://placehold.it/520x342"/>
+                
+                <p>Please join us. Invest in Code of America and help code the next chapter in American history.</p>
+                
+                <p>If your foundation or company is interested in supporting our work, please contact Ashley Meyers, development and engagement manager, to discuss how you can align your support with a key need of our organization, or to learn more about Code for America. Ashley can be reached at 415-236-3796 or ashley@codeforamerica.org. More information on grants, in-kind donations, and corporate sponsorships can be found here.</p>
+                
+                <p>Donations to Code for America, a 501(c)(3) nonprofit and nonpartisan organization, are tax deductible.</p>
+                
+                <?php include("../blocks/patterns/form-donate.html") ?>
+            
+        </main><!--@main-->
+        
+    </div>
+    
+    <?php include("../blocks/global/footer.html") ?>  
+    
+<?php include("../blocks/global/foot.html") ?> 
---
+++ templates/governments.php
@@ -0,0 +1,204 @@
+<?php include("../blocks/global/head.html") ?>
+    
+    <div class="masthead masthead-l">
+        <div class="masthead-hero">
+            <img src="/placeholder/masthead-1.jpg" class="masthead-image"/>
+        </div>
+        <header class="layout-container masthead-header">
+            <?php include("../blocks/patterns/nav-breadcrumb.html") ?>
+            <h1 class="page-title">Citizens</h1>
+        </header>
+    </div>
+    
+    <?php include("../blocks/global/header.html") ?>
+    
+        <main role="main">
+        
+            <section class="layout-container">
+                
+                <nav class="nav-breadcrumbs">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">All cities</a></li>
+                        <li><a href="#">Upcoming</a></li>
+                        <li>2013 Fellows</li>
+                    </ul>
+                </nav>
+            
+                <header class="badge-heading">
+                    <i class="pop icon-rocket"></i>
+                    <h2>The Brigade</h2>
+                </header>
+                
+                <div class="layout-row">
+                
+                    <div class="layout-col layout-colspan-5">
+                    
+                    <p>Boston has been a techie city for a very long time. The city established the nation’s first Mayor’s Office of New Urban Mechanics (MONUM) and the department’s co-chairs were named Governing Magazine’s Public Officials of the Year. With forward-thinking leaders at the helm, Boston is paving the way for civic innovation. The city has an active Code for America Brigade — a group of local volunteers who meet regularly to attend events, collaborate on new projects and stand up existing apps. For details visit Code for Boston.</p>
+                    
+                    </div>
+                
+                    <div class="layout-col layout-colspan-4">
+                    
+                        <img class="teaser-image" src="/placeholder/big-data.jpg">
+                        
+                        <ul class="list-gappy list-inline list-buttons">
+                        
+                            <li><a href="#" class="button button-prominent">Visit code for Boston</a></li>
+                        
+                            <li><a href="#" class="button">Visit Open Data portal</a></li>
+                        
+                        </ul>
+                    
+                    </div>
+                
+                </div>
+                
+            </section>
+            
+            <section class="section-blue">
+                <div class="layout-container">
+                    <div class="highlight highlight-with-avatar">
+                        <img class="highlight-avatar" src="/placeholder/sf-seal.png"/>
+                        <blockquote>
+                            <p>Finding little wins that you can discover along the way, moments when people can come together and solve some small issue — that’s critical to the experience of innovation.</p>
+                        </blockquote>
+                        <p class="attribute">Nigel Jacob, Co-Director <a href="#">Boston’s Office of New Urban Mechanics</a></p>
+                    </div>
+                </div>
+            </section>
+            
+            
+            
+            <section class="layout-container">
+            
+                <header class="badge-heading">
+                    <i class="pop pop-red icon-rocket"></i>
+                    <h2>The 2012 Fellowship in Boston</h2>
+                </header>
+                
+                <p>In 2011, The City of Boston applied for the fellowship program with Microsoft as their community partners. Given a strong Office of New Urban Mechanics, the 2011 fellows Joel Mahoney, Karla Macedo and Talin Salway knew they were dealing with powerful allies. That being said, when the city requested “backpack apps” (an education API and developer platform) — they received so much more.</p>
+                
+                <p>Apps created during that fellowship year help teachers send students text reminders, help parents track the school bus in realtime and help students and parents assess their school eligibility.</p>
+                
+                <p>The DiscoverBPS app has been heralded by the Washington Post as a true feat. Fellows built the still popular application in only 2 months with the bulk of the work falling on the shoulders of developer Joel Mahoney. The app has since been taken over by the City of Boston’s IT department.</p>
+                
+                <p>Some of the other apps built that year include a developer collaboration platform, tool that turns spreadsheets into APIs, mobile app that locates wifi points and a tool that looks a bit like Foursquare for snow-covered fire hydrants called Adopt-a-hydrant.  While “Adopt-a” may seem specific to Boston, it’s since been redeployed to storm drains in Seattle and tsunami sirens in Honolulu.</p>
+            
+            </section>
+            
+            <section class="layout-container">
+                    
+                
+                <header class="badge-heading">
+                    <i class="pop pop-dark-blue icon-rocket"></i>
+                    <h2>2012 Apps</h2>
+                </header>
+                
+                <ul class="list-no-bullets layout-grid">
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">Discover BPS</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">Adopt-a-Hydrant</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">ClassTalk</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">Where's my Schoolbus?</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">TechnoFinder</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">Data Works</strong>
+                        </a>
+                    </li>
+                    <li class="layout-col layout-colspan-3">
+                        <a href="#" class="seal">
+                            <img class="seal-image" src="http://placehold.it/160x50" />
+                            <strong class="seal-label">Data Finder</strong>
+                        </a>
+                    </li>
+                </ul>
+                
+                <p class="heading-mini">This program made possible by these community partners</p>
+                
+                <ul class="list-inline">
+                    <li><a href="#"><img src="/placeholder/logo-microsoft.png"/></a></li>
+                </ul>
+            
+            </section>
+            
+            <div class="section-gray">
+            
+                <section class="layout-container">
+            
+                    <header class="badge-heading">
+                        <i class="pop pop-orange icon-rocket"></i>
+                        <h2>Articles &amp; video</h2>
+                    </header>
+                    
+                    <ul class="layout-grid">
+                        <li class="layout-col layout-colspan-3">
+                            <article class="teaser">
+                                <img class="teaser-image" src="/placeholder/big-data.jpg" />
+                                <div class="teaser-description">
+                                    <a href="#"><h4 class="teaser-title">Technology team is developing smart solutions to everyday city problems</h4></a>
+                                    <div class="teaser-body">
+                                        <p>On a Friday in the middle of February, Michael Hughes, director of the Boston Public Schools Transportation Department, met with seven visiting fellows from Code for America, a new nonprofit organization that matches teams [&hellip;] <a href="#">Read more</a></p>
+                                    </div>
+                                </div>
+                            </article>
+                        </li>
+                        <li class="layout-col layout-colspan-3">
+                            <article class="teaser">
+                                <img class="teaser-image" src="/placeholder/big-data.jpg" />
+                                <div class="teaser-description">
+                                    <a href="#"><h4 class="teaser-title">Technology team is developing smart solutions to everyday city problems</h4></a>
+                                    <div class="teaser-body">
+                                        <p>On a Friday in the middle of February, Michael Hughes, director of the Boston Public Schools Transportation Department, met with seven visiting fellows from Code for America, a new nonprofit organization that matches teams [&hellip;] <a href="#">Read more</a></p>
+                                    </div>
+                                </div>
+                            </article>
+                        </li>
+                        <li class="layout-col layout-colspan-3">
+                            <article class="teaser">
+                                <img class="teaser-image" src="/placeholder/big-data.jpg" />
+                                <div class="teaser-description">
+                                    <a href="#"><h4 class="teaser-title">Technology team is developing smart solutions to everyday city problems</h4></a>
+                                    <div class="teaser-body">
+                                        <p>On a Friday in the middle of February, Michael Hughes, director of the Boston Public Schools Transportation Department, met with seven visiting fellows from Code for America, a new nonprofit organization that matches teams [&hellip;] <a href="#">Read more</a></p>
+                                    </div>
+                                </div>
+                            </article>
+                        </li>
+                    </ul>
+                
+                </section>
+                
+            </div>
+            
+        </main><!--@main-->
+    
+    <?php include("../blocks/global/footer.html") ?>  
+    
+<?php include("../blocks/global/foot.html") ?> 
