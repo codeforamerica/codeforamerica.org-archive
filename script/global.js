d56d803166c1c66e49e106630a26d3f27b79228e
Jeremy Keith <jeremy@adactio.com>
2013-11-26 11:51:16 +0000

    Revert "Setting up JavaScript for small screen nav"
    This reverts commit 37ed3d259e4f0863107cab96e18364f8fccfee3b.

--- script/global.js
+++ script/global.js
@@ -1,19 +1,13 @@
-
+/*
+Add or remove a class on the navigation depending on how far the user has scrolled down.
+In the CSS, this class gets position:fixed within a widescreen media query.
+*/
 (function (win, doc) {
     'use strict';
     if (!doc.querySelector || !win.addEventListener) {
         // doesn't cut the mustard.
         return;
     }
-
-    if (doc.documentElement.clientWidth > 480) {
-        
-    }
-
-/*
-Add or remove a class on the navigation depending on how far the user has scrolled down.
-In the CSS, this class gets position:fixed within a widescreen media query.
-*/
     var toggleClass = 'sticky',
         reg = new RegExp('(\\s|^)' + toggleClass + '(\\s|$)'),
         logo = doc.querySelector('.global-header'),
