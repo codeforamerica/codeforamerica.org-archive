2275a76e23e57518af1b00567464e49249cc5475
Anna Debenham <anna@maban.co.uk>
2013-11-27 14:38:45 +0000

    Added apps and apis template

---
+++ templates/apps-and-apis.php
@@ -0,0 +1,49 @@
+<?php include("../blocks/global/head.html") ?>
+    
+    <div class="masthead masthead-l">
+        <div class="masthead-hero">
+            <img src="/placeholder/masthead-2.jpg" class="masthead-image"/>
+        </div>
+        <header class="layout-container masthead-header">
+            <?php include("../blocks/patterns/nav-breadcrumb.html") ?>
+            <h1 class="page-title" >Apps &amp; APIs</h1>
+        </header>
+    </div>
+    
+    <?php include("../blocks/global/header.html") ?>
+    
+    <main role="main">
+        
+        <section>
+        
+            <div class="layout-container">
+                
+                <div class="layout-centered">
+                    
+                    <h2 class="h3">Our fellowship, accelerator and incubator apps are the product of hundreds of community interviews. In partnership with our government partners, fellows and startup founders figure out how to best serve citizens.</h2>
+                    
+                    <p>For a list of more civic apps check out <a href="#">CfA Commons</a>.</p>
+                                
+                </div>
+            
+            </div>
+            
+        </section>
+        
+        <section>
+        
+            <div class="layout-container">
+                
+                <?php include("../blocks/patterns/teasers.html") ?>
+                
+                <p class="layout-centered"><a href="#" class="button">Load More</a></p>
+            
+            </div>
+            
+        </section>
+        
+    </main><!--@main-->
+    
+    <?php include("../blocks/global/footer.html") ?>  
+    
+<?php include("../blocks/global/foot.html") ?> 
