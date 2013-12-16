797af5713ce6c03830c8669e7288164ba2c601b3
Anna Debenham <anna@maban.co.uk>
2013-12-16 01:05:39 +0000

    Move SVG and JS detection to global.js

--- _script/global.js
+++ _script/global.js
@@ -1,3 +1,12 @@
+// Adds class of js to the html tag if JS is enabled
+document.getElementsByTagName('html')[0].className += ' js';
+
+// Adds class of svg to the html tag if svg is enabled
+(function flagSVG() {
+    var ns = {'svg': 'http://www.w3.org/2000/svg'};
+    if(document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")) {document.getElementsByTagName('html')[0].className += ' svg';}
+})();
+
 (function (win, doc) {
     'use strict';
     if (!doc.querySelector || !win.addEventListener) {
