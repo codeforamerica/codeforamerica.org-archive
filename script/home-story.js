e466c55ddd762e88af0cf8d5f9aa435bdf7c3420
Michal Migurski <mike@teczno.com>
2014-01-28 14:40:14 -0800

    Loading stories.js dynamically to find story content

---
+++ script/home-story.js
@@ -0,0 +1,56 @@
+/*
+ * http://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript/524798#524798
+ *
+ * Here's a script which adds IE-style createStyleSheet() and addRule()
+ * methods to browsers which don't have them.
+ */
+if(typeof document.createStyleSheet === 'undefined')
+{
+    document.createStyleSheet = (function() {
+        function createStyleSheet(href)
+        {
+            if(typeof href !== 'undefined') {
+                var element = document.createElement('link');
+                element.type = 'text/css';
+                element.rel = 'stylesheet';
+                element.href = href;
+            }
+            else {
+                var element = document.createElement('style');
+                element.type = 'text/css';
+            }
+
+            document.getElementsByTagName('head')[0].appendChild(element);
+            var sheet = document.styleSheets[document.styleSheets.length - 1];
+
+            if(typeof sheet.addRule === 'undefined')
+                sheet.addRule = addRule;
+
+            if(typeof sheet.removeRule === 'undefined')
+                sheet.removeRule = sheet.deleteRule;
+
+            return sheet;
+        }
+
+        function addRule(selectorText, cssText, index) {
+            if(typeof index === 'undefined')
+                index = this.cssRules.length;
+
+            this.insertRule(selectorText + ' {' + cssText + '}', index);
+        }
+
+        return createStyleSheet;
+    })();
+}
+
+function on_stories(stories)
+{
+    var story = stories[Math.floor(Math.random() * stories.length)];
+
+    document.getElementById('story-title').innerHTML = story.head;
+    document.getElementById('story-content').innerHTML = story.content;
+    document.getElementById('story-link').href = story.link;
+
+    var s = document.createStyleSheet();
+    s.addRule('.masthead:before', "background-image: url('"+story.image+"')");
+}
---
+++ stories.js
@@ -0,0 +1,10 @@
+on_stories(
+[
+    {
+        "head": "In Oakland",
+        "content": "the OpenOakland group built a participatory budgeting website that attracted XXXXX citizen comments in the 2013 budget process.",
+        "image": "http://farm9.staticflickr.com/8281/7673631824_b1a5b52fa8_k.jpg",
+        "link": "cities/oakland"
+    }
+]
+);
