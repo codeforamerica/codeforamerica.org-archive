88224147202207d1739c3e52d1cc79314022f4e7
Michal Migurski <mike@teczno.com>
2014-01-28 14:45:25 -0800

    Added more stories

--- stories.js
+++ stories.js
@@ -1,10 +1,28 @@
 on_stories(
 [
     {
+        "head_html": "In Philadelphia",
+        "content_html": "a team of geeks built Textizen, reinventing the town hall by enabling thousands to participate in their government via SMS.",
+        "image_src": "http://farm6.staticflickr.com/5450/9787951071_cae6eddc12_b.jpg",
+        "link_href": "cities/philadelphia"
+    },
+    {
+        "head_html": "In San Francisco",
+        "content_html": "one third of HSA clients are unneccessarily cut from food benefits. Four fellows built Promptly offering SMS notifications to keep families fed.",
+        "image_src": "http://farm6.staticflickr.com/5471/10877055376_8ba8f78eef_o.png",
+        "link_href": "cities/sanfrancisco"
+    },
+    {
         "head_html": "In Oakland",
         "content_html": "the OpenOakland group built a participatory budgeting website that attracted XXXXX citizen comments in the 2013 budget process.",
-        "image_src": "http://farm9.staticflickr.com/8281/7673631824_b1a5b52fa8_k.jpg",
+        "image_src": "http://www.nooga.com/assets/8690aaea938051ee58b8679a4e08330f44163.jpg",
         "link_href": "cities/oakland"
+    },
+    {
+        "head_html": "In Honolulu",
+        "content_html": "in 2012, more than 100 citizens and city officials re-wrote the city website in a day. It happened again in Oakland in 2013.",
+        "image_src": "http://farm9.staticflickr.com/8281/7673631824_b1a5b52fa8_k.jpg",
+        "link_href": "cities/honolulu"
     }
 ]
 );
