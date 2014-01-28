23a503b37e068af9dc4ee0fb1ec884c99e333da0
Michal Migurski <mike@teczno.com>
2014-01-28 14:41:52 -0800

    Tweaked field names in stories.js

--- script/home-story.js
+++ script/home-story.js
@@ -43,14 +43,17 @@ if(typeof document.createStyleSheet === 'undefined')
     })();
 }
 
+/*
+ * Stories.js calls on_stories().
+ */
 function on_stories(stories)
 {
     var story = stories[Math.floor(Math.random() * stories.length)];
 
-    document.getElementById('story-title').innerHTML = story.head;
-    document.getElementById('story-content').innerHTML = story.content;
-    document.getElementById('story-link').href = story.link;
+    document.getElementById('story-title').innerHTML = story.head_html;
+    document.getElementById('story-content').innerHTML = story.content_html;
+    document.getElementById('story-link').href = story.link_href;
 
     var s = document.createStyleSheet();
-    s.addRule('.masthead:before', "background-image: url('"+story.image+"')");
+    s.addRule('.masthead:before', "background-image: url('"+story.image_src+"') !important");
 }
--- stories.js
+++ stories.js
@@ -1,10 +1,10 @@
 on_stories(
 [
     {
-        "head": "In Oakland",
-        "content": "the OpenOakland group built a participatory budgeting website that attracted XXXXX citizen comments in the 2013 budget process.",
-        "image": "http://farm9.staticflickr.com/8281/7673631824_b1a5b52fa8_k.jpg",
-        "link": "cities/oakland"
+        "head_html": "In Oakland",
+        "content_html": "the OpenOakland group built a participatory budgeting website that attracted XXXXX citizen comments in the 2013 budget process.",
+        "image_src": "http://farm9.staticflickr.com/8281/7673631824_b1a5b52fa8_k.jpg",
+        "link_href": "cities/oakland"
     }
 ]
 );
