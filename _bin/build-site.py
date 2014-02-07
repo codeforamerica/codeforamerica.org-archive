f05014aba1e094b7807ba88fc2712989d7c1eb23
Michal Migurski <mike@teczno.com>
2014-02-07 14:34:24 -0800

    Added built-it script, in its current state

---
+++ _bin/build-site.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from subprocess import check_call, check_output, PIPE
+from os import chdir
+from sys import argv, stdin
+from json import load
+
+def current_branch():
+    ref = check_output('git symbolic-ref HEAD'.split())
+    return ref.strip().split('/')[-1]
+
+def checkout_ref(ref):
+    print '    git checkout', ref
+    return check_call(('git', 'checkout', '-q', ref), stdout=PIPE)
+
+def jekyll_build(dir):
+    print '    jekyll build to', dir
+    check_call(('jekyll', 'build', '-d', dir), stdout=PIPE)
+
+if __name__ == '__main__':
+
+    builds = load(stdin)
+    build = [b for b in builds if b['result'] == 0][0]
+
+    print '-->', 'Build %(number)s - %(finished_at)s' % build
+
+    commit = build['commit']
+    checkout_dir, build_dir = argv[1:]
+
+    try:
+        chdir(checkout_dir)
+        branch = current_branch()
+
+        checkout_ref(commit)
+        jekyll_build(build_dir)
+    
+    except Exception, e:
+        print 'ERR', e
+
+    finally:
+        checkout_ref(branch)
