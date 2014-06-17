3372c82f2dc7fdf87af91d1ab256cba290d0b472
Michal Migurski <mike@teczno.com>
2014-06-16 22:18:10 -0700

    Updated global navigation links to /governments

--- stories.js
+++ stories.js
@@ -1,25 +1,25 @@
 on_stories(
 [
     {
-        "head_html": "<a href='cities/philadelphia'>In Philadelphia</a>",
+        "head_html": "<a href='governments/philadelphia'>In Philadelphia</a>",
         "content_html": "a team of geeks built <a href='apps/textizen'>Textizen</a>, reinventing the town hall by enabling thousands to participate in their government via SMS.",
         "image source": "http://www.flickr.com/photos/codeforamerica/9787951071/",
         "image_src": "media/images/homepage-stories/philadelphia-textizen.jpg"
     },
     {
-        "head_html": "<a href='cities/sanfrancisco'>In San Francisco</a>",
+        "head_html": "<a href='governments/sanfrancisco'>In San Francisco</a>",
         "content_html": "one third of food stamp clients are unnecessarily cut from benefits. Four fellows built <a href='apps/promptly'>Promptly</a> offering SMS notifications to keep families fed.",
         "image source": "http://www.flickr.com/photos/codeforamerica/10877055376/",
         "image_src": "media/images/homepage-stories/sanfrancisco-promptly.jpg"
     },
     {
-        "head_html": "<a href='cities/oakland'>In Oakland</a>",
+        "head_html": "<a href='governments/oakland'>In Oakland</a>",
         "content_html": "the OpenOakland group built a participatory budgeting app so citizens could influence the 2013 budget process.",
         "image source": "http://www.nooga.com/assets/8690aaea938051ee58b8679a4e08330f44163.jpg",
         "image_src": "media/images/homepage-stories/oakland-budget.jpg"
     },
     {
-        "head_html": "<a href='cities/honolulu'>In Honolulu</a>",
+        "head_html": "<a href='governments/honolulu'>In Honolulu</a>",
         "content_html": "in 2012, more than 100 citizens and city officials re-wrote the city website in a day. It happened again in Oakland in 2013.",
         "image source": "http://www.flickr.com/photos/bytemarks/7673631824/",
         "image_src": "media/images/homepage-stories/honolulu-answers.jpg"
