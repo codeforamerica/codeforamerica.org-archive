dc4fd3ece1eee799d567206426bcc52a5d4aa2ba
ewee <ewee@dogmo.com>
2014-07-07 13:45:26 -0700

    Updates to nav (drop down), content and welcome section. Adds loading div  to all pages.

---
+++ js/simple-loader.js
@@ -0,0 +1,7 @@
+    // Wait for window load
+    $(window).load(function() {
+      // Adds min display time and animation
+		$("#loading").animate({
+          opacity:0,
+     	  }, 1200).hide(1200);
+    });
