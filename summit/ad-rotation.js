7494b2d46ae74d5aeb8d0c69a21b6731e4edfc68
mattboitan0 <mattb@codeforamerica.org>
2014-09-08 10:49:19 -0700

    added microsoft link too

--- summit/ad-rotation.js
+++ summit/ad-rotation.js
@@ -3,5 +3,5 @@ if(Math.random() < 0.5) {
     document.write('<section style="text-align: center; padding: 1em; background-color: #ffffff"><a href="http://developer.accela.com"><img src="/summit/img/accela.jpg" alt="accela" width="600" height="100"></a></section>');
 
 } else {
-    document.write('<section style="text-align: center; padding: 1em; background-color: #58595b"><img src="/summit/img/microsoft.jpg" alt="microsoft" width="600" height="100"></section>');
+    document.write('<section style="text-align: center; padding: 1em; background-color: #58595b"><a href="http://www.microsoft.com"><img src="/summit/img/microsoft.jpg" alt="microsoft" width="600" height="100"></a></section>');
 }
