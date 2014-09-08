00adf8317ce466f332eaa362f36f787097d5d124
mattboitan0 <mattb@codeforamerica.org>
2014-09-08 10:42:36 -0700

    tried to add link to accela banner ad

--- summit/ad-rotation.js
+++ summit/ad-rotation.js
@@ -1,6 +1,6 @@
 // "share of voice"
 if(Math.random() < 0.5) {
-    document.write('<section style="text-align: center; padding: 1em; background-color: #ffffff"><img src="/summit/img/accela.jpg" alt="accela" width="600" height="100"></section>');
+    document.write('<section style="text-align: center; padding: 1em; background-color: #ffffff"><img src="/summit/img/accela.jpg" href="http://developer.accela.com" alt="accela" width="600" height="100"></section>');
 
 } else {
     document.write('<section style="text-align: center; padding: 1em; background-color: #58595b"><img src="/summit/img/microsoft.jpg" alt="microsoft" width="600" height="100"></section>');
