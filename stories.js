5c308defad6b63cd04260718646a607e9de97b57
davidrleonard <dleonard@codeforamerica.org>
2014-12-13 00:30:07 -0800

    Comment out the other stories, add in main story about singing up to be fellowship gov

--- stories.js
+++ stories.js
@@ -1,34 +1,41 @@
 on_stories(
 [
+    // {
+    //     "head_html": "<a href='governments/philadelphia'>In Philadelphia</a>",
+    //     "content_html": "a team of geeks built <a href='apps/textizen'>Textizen</a>, reinventing the town hall by enabling thousands to participate in their government via SMS.",
+    //     "image source": "http://www.flickr.com/photos/codeforamerica/9787951071/",
+    //     "image_src": "media/images/homepage-stories/philadelphia-textizen.jpg"
+    // },
+    // {
+    //     "head_html": "<a href='governments/sanfrancisco'>In San Francisco</a>",
+    //     "content_html": "one third of food stamp clients are unnecessarily cut from benefits. Four fellows built <a href='apps/promptly'>Promptly</a> offering SMS notifications to keep families fed.",
+    //     "image source": "http://www.flickr.com/photos/codeforamerica/10877055376/",
+    //     "image_src": "media/images/homepage-stories/sanfrancisco-promptly.jpg"
+    // },
+    // {
+    //     "head_html": "<a href='governments/oakland'>In Oakland</a>",
+    //     "content_html": "the OpenOakland group built a participatory budgeting app so citizens could influence the 2013 budget process.",
+    //     "image source": "http://www.nooga.com/assets/8690aaea938051ee58b8679a4e08330f44163.jpg",
+    //     "image_src": "media/images/homepage-stories/oakland-budget.jpg"
+    // },
+    // {
+    //     "head_html": "<a href='/about'>For the past four years</a>",
+    //     "content_html": "in partnership with governments, communities, and companies, Code for America has built technology to address important community issues.",
+    //     "image_src": "media/images/homepage-stories/Beta-Tiffani-Fumi-some-guy.jpg"
+    // },
+    // {
+    //     "head_html": "<a href='http://www.codeforamerica.org/blog/2014/11/20/announcing-the-2015-fellows'>Announcing the 2015 Fellows</a>",
+    //     "content_html": "Meet the 24 fellows who are taking leave from jobs at tech companies, their own businesses, and government, to use their skills for good during a year of public service.",
+    //     "image_src": "media/images/homepage-stories/point-bonita.png",
+    //     "box_link": "http://www.codeforamerica.org/blog/2014/11/20/announcing-the-2015-fellows",
+    //     "box_text": "Read the announcement"
+    // },
     {
-        "head_html": "<a href='governments/philadelphia'>In Philadelphia</a>",
-        "content_html": "a team of geeks built <a href='apps/textizen'>Textizen</a>, reinventing the town hall by enabling thousands to participate in their government via SMS.",
-        "image source": "http://www.flickr.com/photos/codeforamerica/9787951071/",
-        "image_src": "media/images/homepage-stories/philadelphia-textizen.jpg"
-    },
-    {
-        "head_html": "<a href='governments/sanfrancisco'>In San Francisco</a>",
-        "content_html": "one third of food stamp clients are unnecessarily cut from benefits. Four fellows built <a href='apps/promptly'>Promptly</a> offering SMS notifications to keep families fed.",
-        "image source": "http://www.flickr.com/photos/codeforamerica/10877055376/",
-        "image_src": "media/images/homepage-stories/sanfrancisco-promptly.jpg"
-    },
-    {
-        "head_html": "<a href='governments/oakland'>In Oakland</a>",
-        "content_html": "the OpenOakland group built a participatory budgeting app so citizens could influence the 2013 budget process.",
-        "image source": "http://www.nooga.com/assets/8690aaea938051ee58b8679a4e08330f44163.jpg",
-        "image_src": "media/images/homepage-stories/oakland-budget.jpg"
-    },
-    {
-        "head_html": "<a href='/about'>For the past four years</a>",
-        "content_html": "in partnership with governments, communities, and companies, Code for America has built technology to address important community issues.",
-        "image_src": "media/images/homepage-stories/Beta-Tiffani-Fumi-some-guy.jpg"
-    },
-    {
-        "head_html": "<a href='http://www.codeforamerica.org/blog/2014/11/20/announcing-the-2015-fellows'>Announcing the 2015 Fellows</a>",
-        "content_html": "Meet the 24 fellows who are taking leave from jobs at tech companies, their own businesses, and government, to use their skills for good during a year of public service.",
-        "image_src": "media/images/homepage-stories/point-bonita.png",
-        "box_link": "http://www.codeforamerica.org/blog/2014/11/20/announcing-the-2015-fellows",
-        "box_text": "Read the announcement"
+        "head_html": "<a href='/governments/fellowship'>Enlist the Fellows</a>",
+        "content_html": "Want to bring the Fellows to work in your government? Catch up and learn about how to partner with us in 2015.",
+        "image_src": "media/images/about_fellowship/longbeach.jpg",
+        "box_link": "/governments/fellowship",
+        "box_text": "Tell me more"
     }
 ]
 );
