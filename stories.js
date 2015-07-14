b0865f476be3bb03f50e72d5788939eb4f617630
ladyson <lauren@codeforamerica.org>
2015-07-14 11:48:52 -0700

    Update stories.js

--- stories.js
+++ stories.js
@@ -1,39 +1,11 @@
 on_stories(
 [
     {
-        "page_link"     : "http://awards.codeforamerica.org/",
-        "title"         : "Announcing the 2015 Code for America Technology Awards",
-        "content_html"  : "The Code for America Technology Awards recognize outstanding products and implementations of government technology.",
+        "page_link"     : "http://codeforamerica.org/about",
+        "title"         : "We need you to make government work for the people, by the people, in the 21st century.",
+        "content_html"  : "TGot an hour? Got a year? There’s a way for you to make a difference.",
         "image_src"     : "/media/images/homepage-stories/awards-header-photo.jpg",
-        "button_text"   : "Learn more"
+        "button_text"   : "Learn more about our work"
     },
-    {
-        "page_link"     : "/geeks/fellowship-apply",
-        "title"         : "The hardest job you'll ever love",
-        "content_html"  : "Apply to be a Code for America fellow.",
-        "image_src"     : "/media/images/about_fellowship/longbeach.jpg",
-        "button_text"   : "Learn more"
-    },
-    {
-        "page_link"     : "/our-work/focus-areas/health/",
-        "title"         : "Delivering health services with dignity, by default",
-        "content_html"  : "Code for America is partnering with local governments to build digital health services focused on users.",
-        "image_src"     : "/media/images/health/somerville.jpg",
-        "button_text"   : "Learn more"
-    },
-    {
-        "page_link"     : "/summit",
-        "title"         : "Roll up your sleeves at the 2015 CFA Summit",
-        "content_html"  : "For three days, more than 1,300 government leaders, technologists, and community members will delve into how we can transform government together.",
-        "image_src"     : "/media/images/summit/2015/summit-selfie.jpg",
-        "button_text"   : "Join in"
-    },
-    {
-        "page_link"     : "/our-work/initiatives/talent",
-        "title"         : "There has never been a better time to work on technology inside government.",
-        "content_html"  : "Engineers, designers, product managers, data scientists, and more &mdash; put your skills to work in service to your country. Let’s bring government into the 21st century together.",
-        "image_src"     : "/media/images/talent/space.jpg",
-        "button_text"   : "Serve your government"
-    }
 ]
 );
