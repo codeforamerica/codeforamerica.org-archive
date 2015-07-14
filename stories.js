65b92be46b9a1da87118c70d21e6cff0559dc699
ladyson <lauren@codeforamerica.org>
2015-07-14 11:49:25 -0700

    Update stories.js

--- stories.js
+++ stories.js
@@ -3,7 +3,7 @@ on_stories(
     {
         "page_link"     : "http://codeforamerica.org/about",
         "title"         : "We need you to make government work for the people, by the people, in the 21st century.",
-        "content_html"  : "TGot an hour? Got a year? There’s a way for you to make a difference.",
+        "content_html"  : "Got an hour? Got a year? There’s a way for you to make a difference.",
         "image_src"     : "/media/images/homepage-stories/awards-header-photo.jpg",
         "button_text"   : "Learn more about our work"
     },
