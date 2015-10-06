c163a5ce122e3f2a705f819bcdb0aa3551579d8c
ZoeBlumenfeld <zoe@codeforamerica.org>
2015-10-06 11:10:32 -0700

    update homepage story

--- stories.js
+++ stories.js
@@ -1,11 +1,11 @@
 on_stories(
 [
-    {
-        "page_link"     : "/summit/#livestream",
-        "title"         : "Watch the 2015 Summit Live Stream",
-        "content_html"  : "More than 1,300 government leaders, technologists and community members are settling in for the 2015 Summit in Oakland. Watch all the main stage speakers live each morning.",
+      {
+        "page_link"     : "http://www.codeforamerica.org/blog/category/cfasummit/",
+        "title"         : "Favorite Moments of the #CfASummit",
+        "content_html"  : "More than 1,300 government leaders, technologists and community members gathered in Oakland to transform government for the 21st century.",
         "image_src"     : "/media/images/summit/2015/summit-selfie.jpg",
-        "button_text"   : "Watch the live stream"
+        "button_text"   : "Read the recaps"
     },
 ]
 );
