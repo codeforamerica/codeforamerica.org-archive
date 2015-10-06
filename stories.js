4bae37522a62955d71c76fdb0dbf02d27d5d524f
davidrleonard <dleonard@codeforamerica.org>
2015-10-06 13:18:16 -0700

    Change up th stories a bit more

--- stories.js
+++ stories.js
@@ -1,11 +1,18 @@
 on_stories(
 [
       {
-        "page_link"     : "http://www.codeforamerica.org/blog/category/cfasummit/",
+        "page_link"     : "/blog/category/cfasummit/",
         "title"         : "Favorite Moments of the #CfASummit",
         "content_html"  : "More than 1,300 government leaders, technologists and community members gathered in Oakland to transform government for the 21st century.",
         "image_src"     : "/media/images/summit/2015/summit-selfie.jpg",
         "button_text"   : "Read the recaps"
     },
+    {
+      "page_link"     : "/brigade/",
+      "title"         : "Volunteer for your community",
+      "content_html"  : "Join community organizers, designers and developers in your city working on important local problems. Raise your voice and your hand.",
+      "image_src"     : "/media/images/about/brigade.jpg",
+      "button_text"   : "Join a Brigade"
+    }
 ]
 );
