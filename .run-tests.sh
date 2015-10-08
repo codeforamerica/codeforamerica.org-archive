11b698c2123d1e4395567fd59d4a0fbce9386982
davidrleonard <dleonard@codeforamerica.org>
2015-10-08 12:23:25 -0700

    Exclude certain pages for percy, load a testing css snippet for percy that stops animations

--- .run-tests.sh
+++ .run-tests.sh
@@ -2,5 +2,5 @@
 jekyll build
 ./.test-httpd.py
 # For percy testing, remove a lot of files we don't need to regression test, then send to percy
-jekyll build && find _site -type f | egrep "(people/|brigade-training/|peer-network-training/|summit/schedule/|summit/speakers/).*html$" | xargs rm
+jekyll build --config _config.yml,_config-testing.yml && find _site -type f | egrep "(people/|brigade-training/|peer-network-training/|summit/schedule/|summit/speakers/|geeks/our-geeks/alumni-fellows/).*\.html$" | xargs rm
 percy snapshot _site --threads 2
